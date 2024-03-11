17b55a6e2c22d9ec1dc3fe704f6bb02e89f26fbb
Elze0 <113585765+Elze0@users.noreply.github.com>
2024-03-11 12:12:41 +0100

    Try8

--- script.js
+++ script.js
@@ -57,3 +57,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 });
+
+ function toggleMenu() {
+    var menuContent = document.getElementById("menuContent");
+    if (menuContent.style.display === "block") {
+      menuContent.style.display = "none";
+    } else {
+      menuContent.style.display = "block";
+    }
+  }
+
+
